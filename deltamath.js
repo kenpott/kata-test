add7d9471d6187174f89883e8216586eb0eab190
KuyaKen <92195844+kenpott@users.noreply.github.com>
2025-08-30 16:00:54 -0700

    Update deltamath.js

--- deltamath.js
+++ deltamath.js
@@ -418,11 +418,15 @@
     dragElement(popup);
 
     function dragElement(elmnt) {
-      var pos1 = 0,
+      let pos1 = 0,
         pos2 = 0,
         pos3 = 0,
         pos4 = 0;
+
+      // Desktop
       elmnt.onmousedown = dragMouseDown;
+      // Mobile
+      elmnt.ontouchstart = dragTouchStart;
 
       function dragMouseDown(e) {
         if (
@@ -430,14 +434,31 @@
           e.target.tagName === "TEXTAREA" ||
           e.target.tagName === "BUTTON" ||
           e.target.isContentEditable
-        ) {
+        )
           return;
-        }
+
         e.preventDefault();
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
         document.onmousemove = elementDrag;
+      }
+
+      function dragTouchStart(e) {
+        if (
+          e.target.tagName === "INPUT" ||
+          e.target.tagName === "TEXTAREA" ||
+          e.target.tagName === "BUTTON" ||
+          e.target.isContentEditable
+        )
+          return;
+
+        e.preventDefault();
+        const touch = e.touches[0];
+        pos3 = touch.clientX;
+        pos4 = touch.clientY;
+        document.ontouchend = closeDragElement;
+        document.ontouchmove = elementDragTouch;
       }
 
       function elementDrag(e) {
@@ -450,9 +471,22 @@
         elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
       }
 
+      function elementDragTouch(e) {
+        e.preventDefault();
+        const touch = e.touches[0];
+        pos1 = pos3 - touch.clientX;
+        pos2 = pos4 - touch.clientY;
+        pos3 = touch.clientX;
+        pos4 = touch.clientY;
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+      }
+
       function closeDragElement() {
         document.onmouseup = null;
         document.onmousemove = null;
+        document.ontouchend = null;
+        document.ontouchmove = null;
       }
     }
 
